b5348cd2a7c6a2cddf6a52502f65a86cda4a50cc
吴静波 <wujingbo@noahwm.com>
2016-05-10 13:50:48 +0800

    添加widget生成器

---
+++ src/widget/index.js
@@ -0,0 +1,29 @@
+import yeoman from 'yeoman-generator';
+import _ from 'underscore.string';
+
+class WidgetGenerator extends yeoman.Base {
+  constructor(...args) {
+    super(...args);
+
+    this.argument('name', {
+      desc: 'Widget name, e.g. list, progressBar, etc.',
+      type: String,
+      required: true
+    });
+
+    this.destinationRoot('src');
+    this.name = _.classify(this.name);
+  }
+
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath('widget.ejs'),
+      this.destinationPath('component/widget', `${this.name}.js`),
+      {
+        widget: this.name
+      }
+    );
+  }
+}
+
+export default WidgetGenerator;
